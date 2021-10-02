372afe9fa6241f22d82c75f7179ac909042bdf4c
Neeraj <76809680+neeraj395@users.noreply.github.com>
2021-10-02 21:39:40 +0530

    ....

--- js/Code.js
+++ js/Code.js
@@ -8,7 +8,7 @@ function clues() {
     textSize(15)
     text("R E V B A I L A", 100,50)
     fill("lightblue")
-    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: Always changing, not constant TEST 123  !!", 100,70)
 
     fill("white")
     textSize(15)
